8a0ee60dc3a7cdbb67261e35e1a94f2695e1030d
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-04 17:20:02 +0530

    5th exercise complete.all test cases cleared

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(ele1, ele2) {
+    if (typeof ele1 != "number" || typeof ele2 != "number") {
+        return "ERROR";
+    };
+    if (ele1 < 0) {
+        return "ERROR";
+    };
+    if (ele2 < ele1) {
+        let temp = ele1;
+        ele1= ele2;
+        ele2 = temp;
+    };
+    sum = 0;
+    while (ele1 <= ele2) {
+        sum += ele1;
+        ele1 += 1;
+    };
+    return sum
+
+
 
 };
 
