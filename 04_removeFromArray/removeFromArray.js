b5c230ab680595b4d84d4e653a08ec790682e0af
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-04 17:07:49 +0530

    4th exercise complete. all test cases cleared

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    j = 1;
+    while (arguments[j] != undefined) {
+        ele = arguments[j];
+        console.log(ele);
+        for (i = 0; i < array.length; i ++) {
+            if (array[i] === ele) {
+                array.splice(i, 1);
 
+            }
+        }
+        j += 1;
+
+       
+    };
+    return array;
 };
 
+//array = removeFromArray([1, 2, 3], "1", 3);
+//console.log(array)
 // Do not edit below this line
 module.exports = removeFromArray;
