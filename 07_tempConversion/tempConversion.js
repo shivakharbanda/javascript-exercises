41ddd31f5dc39d10fad3e24ee17514435390e1ed
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-05 14:28:37 +0530

    7th exercise done. All testcase cleared. fundamentals Part4 exercises done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
 const ftoc = function() {
-
+ let f = arguments[0]
+ let c = (f - 32)*(5/9);
+  return Math.round(c * 10) / 10;
 };
 
 const ctof = function() {
-
+  let c = arguments[0];
+  let f = (c * (9/5)) + 32
+  return Math.round(f * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
