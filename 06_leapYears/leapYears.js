a57fdd631f6127f7586040e036920678317cab95
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-05 13:29:10 +0530

    exercise 6 finished. All test cases are clear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
 const leapYears = function() {
+    let year = parseInt(arguments[0]);
+    let result = false
+    if (year % 4 == 0 ) {
+        result = true;
+        if (year % 100 == 0){
+            result = false;
+            if (year % 400 == 0) {
+                result = true
+            }
+        }
+    }
 
+    return result
 };
-
+console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
