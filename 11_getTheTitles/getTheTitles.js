ab30bd93514707650c58b4061809ab5d39f7103c
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-12 13:01:36 +0530

    11th get the title done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
 const getTheTitles = function() {
-
+    arr = arguments[0];
+    titles = []
+    arr.forEach(element => {
+        titles.push(element.title);
+    });
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+console.log(getTheTitles( [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]));
