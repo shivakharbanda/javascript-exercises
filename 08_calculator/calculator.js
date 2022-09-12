8ebfa813f2d6ae203e89f08ab70f34490d620988
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-12 11:14:17 +0530

    exercise 08_calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,60 @@
 const add = function() {
-	
+	addition = 0;
+  args = [...arguments];
+
+  args.forEach(element => {
+    addition += Number(element);
+  });
+
+  return addition;
+
 };
 
 const subtract = function() {
-	
+	arg1 = arguments[0];
+
+  args = [...arguments];
+
+  args.forEach(element => {
+    if (element == arg1) {
+      console.log("do nothing");
+    } else {
+      arg1 -= element;
+    }
+  });
+  return arg1;
 };
 
 const sum = function() {
-	
+	addition = 0;
+  arguments[0].forEach(element =>  {
+    addition += Number(element);
+  });
+  return addition;
 };
 
 const multiply = function() {
+  args = arguments[0];
+  product = 1;
 
+  args.forEach(element => {
+    //console.log(product, element);
+    product = product * Number(element);
+  });
+  return product;
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	num = Number(arguments[0]);
+  if (num === 0 || num === 1) {
+    return 1;
+  };
+  return num * factorial(num - 1);
 };
-
 // Do not edit below this line
 module.exports = {
   add,
