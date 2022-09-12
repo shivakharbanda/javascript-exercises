b70ebd4598d3037306811d00f5fc14b92b5a52a3
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-12 12:07:16 +0530

    exercise 09 palindrome complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
 const palindromes = function () {
+    string = arguments[0];
+    let processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+
+    return processedString.split("").reverse().join("") === processedString;
+    
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//
+//console.log(palindromes("A car, a man, a maracas."))
